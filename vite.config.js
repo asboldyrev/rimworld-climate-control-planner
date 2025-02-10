24a29619fd88d0efcfe156b5073587b89afcbfe3
asboldyrev <angelcorpc@ya.ru>
2025-02-10 21:43:09 +0700

    Обновлена конфигурация Vite: добавлены алиасы и настройки сборки

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/rimworld-climate-control-planner/',
   build: {
     outDir: 'dist',
   }
